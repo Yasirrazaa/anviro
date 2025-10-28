d1cec8377406b1ff664e06c6e46cb3eaeb97013e
Yasir Raza <yasirabdali6@gmail.com>
2025-10-28 15:45:14 +0500

    restructured src dir

--- src/ner.py
+++ src/ner.py
@@ -1,7 +1,25 @@
 import spacy
 from transformers import pipeline
 import argparse
-from summarize import summarize_text
+from google import genai
+import os
+
+
+def summarize_text(text):
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"Provide a tag list (3-5 max)  and a summary from following text:\n\n{text} in this fromat:\nTags: [tag1, tag2, tag3] \nSummary: summary text",
+    )
+
+    try:
+        tags= response.text.split("Tags:")[1].split("Summary:")[0].strip()
+        summary = response.text.split("Summary:")[1].strip()
+        return {"tags": tags, "summary": summary}
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return {"tags": response.text, "summary": response.text}
+
 
 def ner_spacy(text):
     nlp = spacy.load("en_core_web_sm")
@@ -15,6 +33,11 @@ def ner_transformers(text):
     entities = [(entity['word'], entity['entity_group']) for entity in ner(text)]
     response = summarize_text(text)
     return {"entities": entities, "tags": response["tags"], "summary": response["summary"]}
+
+
+
+
+
 
 def main():
     argparser = argparse.ArgumentParser(description="Named Entity Recognition using SpaCy or Transformers")
